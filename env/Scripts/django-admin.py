48542b6b2112f1170a3d43ccc1f0dc4d1cc9e92d
fabianhaef <fabian.haefi@gmail.com>
2020-07-14 23:07:38 +0200

    initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\fabia\Documents\Projekte\project_bp\amphibian_ecommerce\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

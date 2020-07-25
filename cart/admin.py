db8b531a126e804051de5ccfa2043e9d1496e945
fabianhaef <fabian.haefi@gmail.com>
2020-07-25 18:17:49 +0200

    checkoutview finished

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Product, OrderItem, Order, LicenceVariation
+from .models import (
+    Product, OrderItem, Order, LicenceVariation, Address
+)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'zip_code',
+        'address_type',
+    ]
+
 
 admin.site.register(Product)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(LicenceVariation)
+admin.site.register(Address, AddressAdmin)
--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product-list'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
     path('<slug>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('cart', views.CartView.as_view(), name='summary'),
     path('remove-from-cart/<pk>', views.RemoveFromCartView.as_view(), name='remove-from-cart'),
